86a27622deeee4eb940201356400ac531fff03d9
Geraldo Guiesi Junior <geraldo.guiesi@gmail.com>
2012-04-04 00:57:36 -0300

    inclusão de módulo responsável por comunicar com box.com

---
+++ boxdotcom.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Apr 4, 2012
+
+@author: geraldo
+'''
+from parser_xml import MyParser
+import requests
+
+
+class BoxDotCom:
+    url = 'https://www.box.net/api/1.0/rest?%s'
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+        
+    def getTicket(self):
+        complement = 'action=get_ticket&api_key=%s' % self.api_key
+        getTicket = self.url % complement
+        result = requests.get(getTicket)
+        a = MyParser()
+        a.parse(result.text)
+        return result.text
+    
+    def login(self):
+        pass
+    
+    def logout(self):
+        pass
+    
+    def get_account_info(self):
+        pass
+    
+    
+api_key = '20c7b3zjjt5g66ermxiarf35lcs24pcz'
+test = BoxDotCom(api_key)
+test.getTicket()
---
+++ parser_xml.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Apr 4, 2012
+
+@author: geraldo
+'''
+from xml.dom.minidom import parse, parseString
+
+class MyParser:
+    
+    def parse(self, result):
+        dom1 = parseString(result)
+        return dom1
