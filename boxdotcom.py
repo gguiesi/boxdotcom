7ca8922a7cbc5f80f72f7facfad06e2d5f309a4e
Geraldo Guiesi <geraldo.guiesi@gmail.com>
2012-04-17 01:15:29 -0300

    inclusão de get_account_tree

--- boxdotcom.py
+++ boxdotcom.py
@@ -5,12 +5,15 @@ Created on Apr 4, 2012
 @author: geraldo
 '''
 from parser_xml import MyParser
+import os
 import requests
 
 class BoxDotCom:
     service_url = 'https://www.box.net/api/1.0/rest?%s'
     ticket = None
-
+    api_key = '20c7b3zjjt5g66ermxiarf35lcs24pcz'
+    auth_token = None
+    
     def __init__(self, api_key):
         self.api_key = api_key
     
@@ -18,7 +21,7 @@ class BoxDotCom:
     Esse método pega a ticket e o usa para fazer todos os serviços.
     Após possuir o ticket é necessário validá-lo
     '''
-    def getTicket(self):
+    def get_ticket(self):
         parser = MyParser()
         complement = 'action=get_ticket&api_key=%s' % self.api_key
         request_url = self.service_url % complement
@@ -28,24 +31,30 @@ class BoxDotCom:
         return self.ticket
     
     # Deve ser usado o serviço: http://developers.box.net/w/page/12923930/ApiFunction_get_auth_token#REST
-    def login(self):
+    def get_auth_token(self):
         parser = MyParser()
-        if self.ticket == None:
-            self.getTicket()
+#        if self.ticket == None:
+#            self.getTicket()
+#            self.validateTicket()
             # fazer um método para abrir um browser e validar a senha.
         complement = 'action=get_auth_token&api_key=%s&ticket=%s' % (self.api_key, self.ticket)
         request_url = self.service_url % complement
         result = requests.get(request_url)
         self.auth_token = parser.getAuthToken(result.text)
+        return self.auth_token
     
     def logout(self):
         self.ticket = None
     
-    def get_account_info(self):
-        pass
+    def get_account_tree(self, api_key=api_key, auth_token=auth_token, folder_id=0, params='onelevel'):
+        url = '%saction=get_account_tree&api_key=%s&auth_token=%s&folder_id=%s&params=%s' % (self.service_url, api_key, auth_token, folder_id, params)
+        result = requests.get(url)
+        return result.text
     
+    def validateTicket(self):
+        url = 'https://www.box.net/api/1.0/auth/%s' % self.ticket
+        os.system('/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args %s' % url)
     
-api_key = '20c7b3zjjt5g66ermxiarf35lcs24pcz'
-test = BoxDotCom(api_key)
-#test.getTicket()
-print test.login()
+#api_key = '20c7b3zjjt5g66ermxiarf35lcs24pcz'
+#test = BoxDotCom(api_key)
+#print test.login()
